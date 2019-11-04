d3c3f51376e2f2ab255e1152fdc4e82b8e3fea18
HyBeard <Kaptsevich.i@gmail.com>
2019-11-05 00:15:00 +0300

    feat: add control & modifier keys data

---
+++ src/js/keysData/controlKeys.js
@@ -0,0 +1,13 @@
+const controlKeysLayout = {
+  Backspace: 'Backspace',
+  Tab: 'Tab',
+  Delete: 'Del',
+  Enter: 'Enter',
+  Space: 'Space',
+  ArrowUp: 'Up',
+  ArrowLeft: 'Left',
+  ArrowDown: 'Dwn',
+  ArrowRight: 'Rgt',
+};
+
+export default controlKeysLayout;
---
+++ src/js/keysData/modifierKeys.js
@@ -0,0 +1,12 @@
+const modifierKeys = {
+  CapsLock: 'Caps Lock',
+  ShiftLeft: 'Shift',
+  ShiftRight: 'Shift',
+  ControlLeft: 'Ctrl',
+  MetaLeft: 'Win',
+  AltLeft: 'Alt',
+  AltRight: 'Alt',
+  ControlRight: 'Ctrl',
+};
+
+export default modifierKeys;
